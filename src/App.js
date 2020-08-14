891f930ae752966050cc8cfbb01948e8ce3cb28f
YounesZdDz <fy_zadi@esi.dz>
2020-08-14 02:37:09 +0100

    feat(App): added shade slider in color palette

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Palette from './components/Palette/Palette';
 import seedColors from './constants/seedColors'
+import { generatePalette } from "./utils/paletteGenerator";
 import './index.css';
 
 function App() {
   return (
     <div>
-      <Palette {...seedColors[4]} />
+      <Palette palette={generatePalette(seedColors[4])} />
     </div>
   );
 }
--- src/components/Palette/Palette.js
+++ src/components/Palette/Palette.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 import ColorBox from '../ColorBox/ColorBox';
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 import './Palette.css'
 
 class Palette extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { level: 500 };
+        this.changeLevel = this.changeLevel.bind(this);
+    }
+    changeLevel(level) {
+        this.setState({ level });
+    }
     render() {
-        const colors = this.props.colors.map((color) => (
-            <ColorBox background={color.color} name={color.name}/> 
+        const { colors } = this.props.palette;
+        const { level } = this.state;
+        const colorBoxes = colors[level].map(color => (
+          <ColorBox background={color.hex} name={color.name} />
         ))
         return (
             <div className='Palette'>
                 {/* header goes here */}
+                <Slider className='slider'
+                    defaultValue={level}
+                    min={100}
+                    max={900}
+                    step={100}
+                    onAfterChange={this.changeLevel}
+                    />
                 <div className='Palette-colors'>
-                    {colors}
+                    {colorBoxes}
                 </div>
                 {/* footer goes here */}
             </div>
