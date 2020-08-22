ff6f87e78559fad1592d29ea92b72a4c5c4bef88
YounesZdDz <fy_zadi@esi.dz>
2020-08-22 19:07:51 +0100

    style: add new palette form style tweaks

--- src/components/PaletteForm/styles.js
+++ src/components/PaletteForm/styles.js
@@ -3,9 +3,6 @@ export default theme => ({
   root: {
     display: 'flex',
   },
-  hide: {
-    display: 'none',
-  },
   drawer: {
     width: DRAWER_WIDTH,
     flexShrink: 0,
@@ -22,11 +19,12 @@ export default theme => ({
     padding: '0 8px',
     ...theme.mixins.toolbar,
     justifyContent: 'flex-end',
+    width: '100%',
   },
   content: {
     flexGrow: 1,
     height: 'calc(100vh - 64px)',
-    padding: theme.spacing.unit * 3,
+    padding: '0',
     transition: theme.transitions.create('margin', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
