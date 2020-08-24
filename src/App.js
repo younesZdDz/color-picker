c41004353e31ca7a9d9e739ba0b051c5f7cf4234
YounesZdDz <fy_zadi@esi.dz>
2020-08-24 11:53:53 +0100

    feat: save palettes to local storage

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ import './index.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { palettes: seedColors };
+    const savedPalettes = JSON.parse(window.localStorage.getItem('palettes'));
+    this.state = { palettes: savedPalettes || seedColors };
     this.savePalette = this.savePalette.bind(this);
     this.findPalette = this.findPalette.bind(this);
   }
@@ -21,9 +22,18 @@ class App extends React.Component {
   }
 
   savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] });
+    this.setState(
+      { palettes: [...this.state.palettes, newPalette] },
+      this.syncLocalStorage,
+    );
   }
-
+  syncLocalStorage() {
+    //save palettes to local storage
+    window.localStorage.setItem(
+      'palettes',
+      JSON.stringify(this.state.palettes),
+    );
+  }
   render() {
     return (
       <Switch>
