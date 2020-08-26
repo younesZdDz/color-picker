47fd010c23cef851a6d71a125f6e1d15400c8502
YounesZdDz <fy_zadi@esi.dz>
2020-08-26 13:20:35 +0100

    fix: fix adding duplicate color when adding radom color

--- src/App.js
+++ src/App.js
@@ -108,6 +108,17 @@ class App extends React.Component {
                     </Page>
                   )}
                 />
+                <Route
+                  render={routeProps => (
+                    <Page>
+                      <PaletteList
+                        palettes={this.state.palettes}
+                        deletePalette={this.deletePalette}
+                        {...routeProps}
+                      />
+                    </Page>
+                  )}
+                />
               </Switch>
             </CSSTransition>
           </TransitionGroup>
